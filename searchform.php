84d5fa132737844084b808faec097fa2215af68b
mikowl <circlemeetssquare@gmail.com>
2013-05-14 05:55:47 -0700

    Value should be search.

--- searchform.php
+++ searchform.php
@@ -1,4 +1,4 @@
 	<form method="get" id="searchform" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
 		<input type="search" class="field" name="s" value="<?php echo esc_attr( get_search_query() ); ?>" id="s" placeholder="Search..." />
-		<input type="submit" class="submit" id="searchsubmit" value="" />
+		<input type="submit" class="submit" id="searchsubmit" value="Search" />
 	</form>
